40c5feef4ff5bde5cb27652188ee53c97e5e3bf5
GarikKane <120460235+GarikKane@users.noreply.github.com>
2023-05-19 12:53:00 +0400

    Add files via upload

---
+++ ласткарта/data.js
@@ -0,0 +1,5 @@
+var Markers = [
+	["Корпус 1", 53.209756, 50.124466, "https://avatars.mds.yandex.net/get-altay/1920875/2a00000174f188fc34e5d0c4f074ae617f93/XXXL"],
+	["Главный корпус",53.208750, 50.124451, "https://avatars.mds.yandex.net/get-altay/1920875/2a00000174f188fc34e5d0c4f074ae617f93/XXXL"],
+	["Корпус 8", 53.209350, 50.125886, "https://avatars.mds.yandex.net/get-altay/1920875/2a00000174f188fc34e5d0c4f074ae617f93/XXXL"]
+	];
